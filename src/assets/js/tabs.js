0e3c1fcd5f1b723aede816e4e044b80f4009b2c4
hayoung <hayoung@dkitec.com>
2024-09-02 09:38:25 +0900

    .

--- src/assets/js/tabs.js
+++ src/assets/js/tabs.js
@@ -1,8 +1,8 @@
 $(function () {
-  var tabs = $(".acl-tabs"),
-    tabList = tabs.find(".acl-tabs__item"),
-    tabLinks = tabList.find(".acl-tabs__link"),
-    tabPanel = tabs.find(".acl-tabs__content");
+  var tabs = $(".tabs"),
+    tabList = tabs.find(".tabs__item"),
+    tabLinks = tabList.find(".tabs__link"),
+    tabPanel = tabs.find(".tabs__content");
 
   // Click event handler for tab links
   tabLinks.on("click", function (e) {
@@ -13,9 +13,9 @@ $(function () {
 
     // Add active class to the clicked tab and remove from siblings
     $parentLi
-      .addClass("acl-is-active")
+      .addClass("is-active")
       .siblings()
-      .removeClass("acl-is-active");
+      .removeClass("is-active");
 
     // Set aria-selected and tabindex for accessibility
     $this
@@ -23,17 +23,17 @@ $(function () {
       .focus() // Set focus on the clicked tab
       .parent()
       .siblings()
-      .find(".acl-tabs__link")
+      .find(".tabs__link")
       .attr({ "aria-selected": false, tabindex: -1 });
 
     // Show the corresponding tab panel and hide others
     tabPanel
       .eq($targetIndex)
-      .addClass("acl-show")
-      .removeClass("acl-hide")
+      .addClass("show")
+      .removeClass("hide")
       .siblings()
-      .removeClass("acl-show")
-      .addClass("acl-hide");
+      .removeClass("show")
+      .addClass("hide");
   });
 
   // Keyboard navigation handler
@@ -44,7 +44,7 @@ $(function () {
 
     if (keycode === 39 || keycode === 40) {
       // Right arrow or Down arrow key
-      var nextTab = $parentLi.next().find(".acl-tabs__link");
+      var nextTab = $parentLi.next().find(".tabs__link");
 
       if (nextTab.length > 0) {
         nextTab.click().focus();
@@ -53,7 +53,7 @@ $(function () {
       }
     } else if (keycode === 37 || keycode === 38) {
       // Left arrow or Up arrow key
-      var prevTab = $parentLi.prev().find(".acl-tabs__link");
+      var prevTab = $parentLi.prev().find(".tabs__link");
 
       if (prevTab.length > 0) {
         prevTab.click().focus();
